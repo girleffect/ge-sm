4caf54485ffd9cd847eb09e720822a1465c777ef
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2019-10-25 15:26:13 +0200

    Update secrets.py

--- ge_sm/common/secrets.py
+++ ge_sm/common/secrets.py
@@ -3,4 +3,4 @@ import os
 # redshift cluster
 rs_pwd = os.environ.setdefault('REDSHIFT_PWD', '****')
 rs_dsn = os.environ.setdefault('REDSHIFT_DSN', '****')
-rs_usr = os.environ.setdefault('redshift_usr', 'dataplatform')
+rs_usr = os.environ.setdefault('REDSHIFT_USR', 'dataplatform')
