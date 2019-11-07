d8062bb75f423454df6b9c7f8b56344c2abf3d26
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2019-11-07 13:33:03 +0200

    update version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ install_requires = [
 ]
 setup(
     name='ge_sm',
-    version='0.0.12',
+    version='0.0.13',
     description="A python applications.",
     long_description=readme,
     author="Praekelt.org",
